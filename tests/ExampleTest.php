e876b823a6fe86b120594908b84c4943d12a85b2
Randall Wilk <22842525+rawilk@users.noreply.github.com>
2020-06-26 09:54:19 -0500

    add return type to example test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -5,7 +5,7 @@ namespace Rawilk\Skeleton\Tests;
 class ExampleTest extends TestCase
 {
     /** @test */
-    public function true_is_true()
+    public function true_is_true(): void
     {
         $this->assertTrue(true);
     }
