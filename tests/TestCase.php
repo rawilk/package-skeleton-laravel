63e402b8394d53b7e2a3c48b463f48bfc5fccaf8
Randall Wilk <wilk.randall@gmail.com>
2020-08-03 12:40:27 -0500

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -30,9 +30,7 @@ class TestCase extends Orchestra
             'prefix' => '',
         ]);
 
-        /*
-        include_once __DIR__.'/../database/migrations/create_skeleton_table.php.stub';
-        (new \CreatePackageTable())->up();
-        */
+        // include_once __DIR__ . '/../database/migrations/create_skeleton_table.php.stub';
+        // (new \CreatePackageTable())->up();
     }
 }
