e5e374eb21aeaefb1c3d1e979876278c25019dd0
Randall Wilk <wilk.randall@gmail.com>
2020-08-03 12:42:29 -0500

    add configuration to phpunit instead of in base test case

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -23,13 +23,6 @@ class TestCase extends Orchestra
 
     public function getEnvironmentSetUp($app)
     {
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-
         // include_once __DIR__ . '/../database/migrations/create_skeleton_table.php.stub';
         // (new \CreatePackageTable())->up();
     }
