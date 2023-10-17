6fbf8e7b1f220ba95b15c705fe65d11e01382af9
Randall Wilk <wilk.randall@gmail.com>
2023-10-17 12:54:55 -0500

    tweak default arch test

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -2,6 +2,26 @@
 
 declare(strict_types=1);
 
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Facade;
+
 it('will not use debugging functions')
-    ->expect(['dd', 'dump', 'ray'])
+    ->expect(['dd', 'dump', 'ray', 'var_dump', 'ddd'])
     ->each->not->toBeUsed();
+
+test('strict types are used')
+    ->expect('VendorName\Skeleton')
+    ->toUseStrictTypes();
+
+test('only facades are placed in the Facades directory')
+    ->expect('VendorName\Skeleton\Facades')
+    ->toBeClasses()
+    ->toExtend(Facade::class)
+    ->classes()
+    ->not->toHaveSuffix('Facade');
+
+test('commands are defined correctly')
+    ->expect('VendorName\Skeleton\Commands')
+    ->toBeClasses()
+    ->toExtend(Command::class)
+    ->toHaveMethod('handle');
